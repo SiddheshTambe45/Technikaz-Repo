6747ef6eeb791a8e9d1f18b4a5de0629e8f9e9e1
SiddheshTambe45 <siddhesh11darkhorse@gmail.com>
2024-12-22 20:36:08 +0530

    page trying to be reolved doc

--- app/[type]/[slug]/page.tsx
+++ app/[type]/[slug]/page.tsx
@@ -4,9 +4,13 @@ import SingleBlogPage from "@/components/SingleBlogPage";
 // import Ads from '@/components/Ads'
 import Upcomings from "@/components/Upcomings";
 
-const page = async ({ params }: { params: Promise<{ slug: string }> }) => {
+type Params = Promise<{ slug: string }>;
+
+const page = async ({ params }: { params: Params }) => {
   // If params is a promise, await it before using it
-  const slug = (await params).slug; // Resolve the promise
+  const resolvedParams = await params; // Resolve the promise
+
+  const slug = resolvedParams.slug;
 
   // const slug = params.slug || ""; // Access `slug` directly from params
   if (!slug) {
